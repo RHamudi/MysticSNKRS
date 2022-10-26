24a1b86f52a12f89e964483c4f605a944e758907
RHamudi <ramonramos.silva16@gmail.com>
2022-10-26 17:42:21 -0300

    fix: deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import type { AppProps } from 'next/app';
 
 import Footer from '../components/footer';
-import Header from '../components/header';
+import Navbar from '../components/header';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
+      <Navbar />
       <Component {...pageProps} />
       <Footer />
     </>
