6c5fbd1d6c892a235567378ce662f5a506de095b
RHamudi <ramonramos.silva16@gmail.com>
2022-10-26 21:33:56 -0300

    fix: style card

--- src/components/card-discover/index.tsx
+++ src/components/card-discover/index.tsx
@@ -4,8 +4,8 @@ export default function CardDiscover(props: {
   productPrice: number;
 }) {
   return (
-    <div className="w-64">
-      <img className="w-64" src={props.productImage} alt="" />
+    <div className="w-64 shadow-md hover:shadow-slate-400 rounded-xl">
+      <img className="w-64" src={props.productImage} alt={props.productName} />
       <div className="flex flex-col items-center">
         <p className="text-lg font-medium">{props.productName}</p>
         <p className="text-lg font-medium">R$ {props.productPrice}</p>
