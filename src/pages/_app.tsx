5e635cd4753f0b95b2518bd5b9f3c8bf3971cf16
RHamudi <ramonramos.silva16@gmail.com>
2022-10-31 21:33:02 -0300

    feat: adding nprogress in the aplication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
 import Footer from '../components/footer';
 import NavbarPage from '../components/navbar';
 import { AuthProvider } from '../contexts/AuthProvider';
 import { queryClient } from '../hooks/use-request';
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
