f23f365c38fcd4eb1548b02a1373e70ff00285d4
RHamudi <ramonramos.silva16@gmail.com>
2022-10-31 21:23:44 -0300

    feat: add types

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,6 +4,7 @@ interface Context {
   username: string | undefined;
   signin: (email: string, password: string) => Promise<void>;
   authenticate: boolean | undefined;
+  logout: VoidFunction;
 }
 
 export const AuthContext = createContext<Context | null>(null);
