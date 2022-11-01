51f242cf4c1151b6b0e56f2a9802f29fbf59fd4d
RHamudi <ramonramos.silva16@gmail.com>
2022-10-31 21:34:16 -0300

    feat: adding types

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -14,9 +14,24 @@ export interface Products {
   __v: number;
 }
 
+export interface ProductsUser {
+  _id: string;
+  productName: string;
+  productDescription: string;
+  productPrice: number;
+  productQuantity: number;
+  productImage: string;
+  username: string;
+  __v: number;
+}
+
 export interface Login {
   msg: string;
   username: string;
   id: string;
   token: string;
+}
+
+export interface CreateUser {
+  msg: string;
 }
