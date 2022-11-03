a529876524934b38c6906be2831e5fbb1630eb59
RHamudi <ramonramos.silva16@gmail.com>
2022-11-03 11:32:00 -0300

    feat: passing properties in cardDiscover

--- src/components/trending-tops/index.tsx
+++ src/components/trending-tops/index.tsx
@@ -1,16 +1,10 @@
-import PlaceholderLoading from 'react-placeholder-loading';
-
 import Link from 'next/link';
 
 import { useProducts } from '../../hooks/useProducts';
 import CardDiscover from '../card-discover';
 
 export default function TrendingTops() {
-  const tops = [
-    'Black Flax',
-    'Air Jordan 2',
-    'Hot Step Air Terra Humara ’99 OG',
-  ];
+  const tops = ['AIR MAX PENNY', 'Air Jordan 6 Retro', 'Air Force 1 Mid'];
 
   const { data: Products, isLoading: Loading } = useProducts();
 
@@ -26,6 +20,7 @@ export default function TrendingTops() {
           {filtered.map((array) =>
             array?.map((filter, index) => (
               <CardDiscover
+                buttonRemove={false}
                 key={index}
                 productId={filter._id}
                 productName={filter.productName}
@@ -43,36 +38,6 @@ export default function TrendingTops() {
       </div>
     );
   } else {
-    return (
-      <div className="flex justify-around flex-wrap gap-24 my-10">
-        <div>
-          <PlaceholderLoading
-            shape="rect"
-            width={256}
-            height={340}
-            colorStart={'rgb(229 231 235)'}
-            colorEnd={'rgb(209 213 219)'}
-          />
-        </div>
-        <div>
-          <PlaceholderLoading
-            shape="rect"
-            width={256}
-            height={340}
-            colorStart={'rgb(229 231 235)'}
-            colorEnd={'rgb(209 213 219)'}
-          />
-        </div>
-        <div>
-          <PlaceholderLoading
-            shape="rect"
-            width={256}
-            height={340}
-            colorStart={'rgb(229 231 235)'}
-            colorEnd={'rgb(209 213 219)'}
-          />
-        </div>
-      </div>
-    );
+    return <div>Carregando</div>;
   }
 }
