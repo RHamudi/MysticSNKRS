22e8fba77cb0eaeef7136a7604764c679a94cd47
Ramon Ramos <94305379+RHamudi@users.noreply.github.com>
2022-11-12 15:20:46 -0300

    fix: types in forms (#4)
    * feat: create page login
    
    * fix: navigation reload page
    
    * feat: create hook context
    
    * feat: create hook request login
    
    * feat: add type login
    
    * feat: wrapping context
    
    * fix: nav links style active
    
    * feat: add next config domain
    
    * feat: add nprogress
    
    * feat: add assets
    
    * feat: using next image
    
    * feat: create page createUser
    
    * fix: css adjustment
    
    * feat: create page home user
    
    * feat: adding next image and nprogress
    
    * feat: css adjustment / verification user auth
    
    * feat: add types
    
    * feat: function logout and save login id
    
    * feat: protected route
    
    * feat: create hooks request
    
    * feat: adding nprogress in the aplication
    
    * feat: create page createUser
    
    * feat: config route protected
    
    * feat: adding types
    
    * fix: adding blur in next image
    
    * feat: add next config imagesize
    
    * feat: add
    properties image size
    
    * feat: adding blur to all images
    
    * fix: resolving navigation
    
    * feat: adding placeholder loading
    
    * feat: add placeholder modal in all pages
    
    * fix: protected route
    
    * feat: component navbar user
    
    * fix: route user product
    
    * fix: adjustment css width
    
    * fix: navigation route user
    
    * feat: remove dependencies
    
    * feat: checking permission to change product
    
    * feat: filtering products
    
    * feat: passing properties in cardDiscover
    
    * feat: passing properties in cardDiscover
    
    * feat: passing properties in cardDiscover
    
    * feat: hook request remove product
    
    * fix: removing element "div" in home page
    
    * feat: create dynamic page
    
    * fix: typing in the image property
    
    * fix: removing unused dependency
    
    * feat:
    
    * feat: create page update product
    
    * feat: hook update product
    
    * feat: type update product
    
    * fix: styles next image
    
    * fix: add trending tops
    
    * fix: removing console log
    
    * fix: url patch
    
    * feat: adding verification in update product
    
    * modified: indentation
    
    * fix: styles
    
    * feat: add load modal
    
    * feat: adding modal loading
    
    * fix: resolving form types

--- src/components/home-user/index.tsx
+++ src/components/home-user/index.tsx
@@ -1,3 +1,5 @@
+import ContentLoader from 'react-content-loader';
+
 import { useUserProducts } from '../../hooks/useProductsUserId';
 import CardDiscover from '../card-discover';
 import NavUser from '../nav-user';
@@ -9,11 +11,76 @@ export default function HomeUser() {
   return (
     <>
       <NavUser />
-      
+
       <div className="flex justify-evenly gap-x-7 gap-y-3 flex-wrap my-9">
 
         {loading === true ? (
-          <div>carregando</div>
+          <div className="flex justify-around flex-wrap gap-20">
+            <ContentLoader
+              foregroundColor="rgb(229 231 235)"
+              backgroundColor="rgb(209 213 219)"
+              width="256"
+              height="340"
+            >
+              <rect x="0" y="0" rx="5" ry="5" width="256" height="340" />
+            </ContentLoader>
+            <ContentLoader
+              foregroundColor="rgb(229 231 235)"
+              backgroundColor="rgb(209 213 219)"
+              width="256"
+              height="340"
+            >
+              <rect x="0" y="0" rx="5" ry="5" width="256" height="340" />
+            </ContentLoader>
+            <ContentLoader
+              foregroundColor="rgb(229 231 235)"
+              backgroundColor="rgb(209 213 219)"
+              width="256"
+              height="340"
+            >
+              <rect x="0" y="0" rx="5" ry="5" width="256" height="340" />
+            </ContentLoader>
+            <ContentLoader
+              foregroundColor="rgb(229 231 235)"
+              backgroundColor="rgb(209 213 219)"
+              width="256"
+              height="340"
+            >
+              <rect x="0" y="0" rx="5" ry="5" width="256" height="340" />
+            </ContentLoader>
+            <ContentLoader
+              foregroundColor="rgb(229 231 235)"
+              backgroundColor="rgb(209 213 219)"
+              width="256"
+              height="340"
+            >
+              <rect x="0" y="0" rx="5" ry="5" width="256" height="340" />
+            </ContentLoader>
+            <ContentLoader
+              foregroundColor="rgb(229 231 235)"
+              backgroundColor="rgb(209 213 219)"
+              width="256"
+              height="340"
+            >
+              <rect x="0" y="0" rx="5" ry="5" width="256" height="340" />
+            </ContentLoader>
+            <ContentLoader
+              foregroundColor="rgb(229 231 235)"
+              backgroundColor="rgb(209 213 219)"
+              width="256"
+              height="340"
+            >
+              <rect x="0" y="0" rx="5" ry="5" width="256" height="340" />
+            </ContentLoader>
+            <ContentLoader
+              foregroundColor="rgb(229 231 235)"
+              backgroundColor="rgb(209 213 219)"
+              width="256"
+              height="340"
+            >
+              <rect x="0" y="0" rx="5" ry="5" width="256" height="340" />
+            </ContentLoader>
+          </div>
         ) : (
           productsUser?.map((product, key) => (
             <CardDiscover
