6b4da6bbf1e873f81d38269526bc662b0306c74d
RHamudi <ramonramos.silva16@gmail.com>
2022-11-16 22:55:49 -0300

    feat: adding form verification

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
+import { Alert } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -11,79 +13,85 @@ import { AuthContext } from '../../contexts/AuthContext';
 
 export default function LoginPage() {
   const auth = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [apiErro, setApiErro] = useState<string>('');
   const router = useRouter();
 
-  function handleEmail(e: any) {
-    setEmail(e.target.value);
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  function handlePassword(e: any) {
-    setPassword(e.target.value);
-  }
-
-  function handleSubmit(e: any) {
-    e.preventDefault();
+  function onSubmit(data: any) {
     NProgress.start();
     auth
-      ?.signin(email, password)
+      ?.signin(data.email, data.password)
       .then(() => {
         NProgress.done();
         router.push('/user/products');
       })
       .catch((err) => {
         NProgress.done();
-        alert(err.response.data.erro);
+        setApiErro(err.response.data.erro);
+        setTimeout(() => {
+          setApiErro('');
+        }, 8000);
       });
   }
 
   return (
-    <div className="flex items-center gap-32">
-      <Image
-        className="w-2/4 -z-50"
-        src={img.src}
-        alt="img"
-        width={655}
-        height={655}
-        blurDataURL={img.blurDataURL}
-      />
-      <form className="flex flex-col">
-        <div>
-          <h1 className="text-3xl font-bold mb-9">Mystic SNKRS</h1>
-        </div>
-        <label className="flex flex-col text-gray-700 text-sm font-bold mb-2">
-          E-mail
-          <input
-            className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            id="email"
-            value={email}
-            onChange={handleEmail}
-          />
-        </label>
-        <label className="flex flex-col text-gray-700 text-sm font-bold mb-2">
-          Password
-          <input
-            className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePassword}
-          />
-        </label>
-        <button
-          type="submit"
-          onClick={handleSubmit}
-          className="w-36 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    <>
+      {apiErro != '' && (
+        <Alert
+          className="w-80 absolute right-0 mt-7 mr-7"
+          variant="outlined"
+          severity="error"
         >
-          Login
-        </button>
-        <p>
-          Doesn't have an account? Register on the website.{' '}
-          <Link href="/create">Click here.</Link>
-        </p>
-      </form>
-    </div>
+          <span>{apiErro}</span>
+        </Alert>
+      )}
+      <div className="flex items-center gap-32">
+        <Image
+          className="w-2/4 -z-50"
+          src={img.src}
+          alt="img"
+          width={655}
+          height={655}
+          blurDataURL={img.blurDataURL}
+        />
+        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <h1 className="text-3xl font-bold mb-9">Mystic SNKRS</h1>
+          </div>
+          <label className="flex flex-col text-gray-700 text-sm font-bold mb-2">
+            E-mail
+            <input
+              className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              {...register('email', { required: true })}
+            />
+            {errors.email && <span>O email é obrigatorio</span>}
+          </label>
+          <label className="flex flex-col text-gray-700 text-sm font-bold mb-2">
+            Password
+            <input
+              className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="password"
+              {...register('password', { required: true })}
+            />
+            {errors.password && <span>A senha é obrigatoria</span>}
+          </label>
+          <button
+            type="submit"
+            className="w-36 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Login
+          </button>
+          <p>
+            Doesn't have an account? Register on the website.{' '}
+            <Link href="/create">Click here.</Link>
+          </p>
+        </form>
+      </div>
+    </>
   );
 }
