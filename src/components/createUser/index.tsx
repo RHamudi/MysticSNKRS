5bc6151826fb09d1321cc0414f57eba56844f528
RHamudi <ramonramos.silva16@gmail.com>
2022-11-16 22:56:04 -0300

    feat: adding form verification

--- src/components/createUser/index.tsx
+++ src/components/createUser/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -7,31 +8,15 @@ import img from '../../assets/login.webp';
 import { createUser } from '../../hooks/useCreate';
 
 export default function CreateUser() {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  function handleUsername(e: React.ChangeEvent<HTMLInputElement>) {
-    setUsername(e.target.value);
-  }
-
-  function handleEmail(e: React.ChangeEvent<HTMLInputElement>) {
-    setEmail(e.target.value);
-  }
-
-  function handlePassword(e: React.ChangeEvent<HTMLInputElement>) {
-    setPassword(e.target.value);
-  }
-
-  function handleSubmit(e: React.MouseEvent<HTMLElement>) {
-    e.preventDefault();
-    createUser(username, email, password)
-      .then((res) => {
-        alert(res.data.msg);
-        router.push('/login');
-      })
-      .catch((err) => alert(err.response.data.erro));
+  function onSubmit(data: any) {
+    console.log(data);
   }
 
   return (
@@ -45,7 +30,7 @@ export default function CreateUser() {
         blurDataURL={img.blurDataURL}
         placeholder="blur"
       />
-      <form className="flex flex-col">
+      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <h1 className="text-3xl font-bold mb-9">Mystic SNKRS</h1>
         </div>
@@ -53,35 +38,28 @@ export default function CreateUser() {
           Username
           <input
             className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsername}
+            {...register('username', { required: true })}
           />
+          {errors.username && <span>Username is required</span>}
         </label>
         <label className="flex flex-col text-gray-700 text-sm font-bold mb-2">
           E-mail
           <input
             className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            id="email"
-            value={email}
-            onChange={handleEmail}
+            {...register('email', { required: true })}
           />
+          {errors.email && <span>E-mail is required</span>}
         </label>
         <label className="flex flex-col text-gray-700 text-sm font-bold mb-2">
           Password
           <input
             className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePassword}
+            {...register('password', { required: true })}
           />
+          {errors.password && <span>Password is required</span>}
         </label>
         <button
           type="submit"
-          onClick={handleSubmit}
           className="w-36 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
           Register
