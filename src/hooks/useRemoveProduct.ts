1872f9606048c2da3938bd4af50e89c4433b3f58
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-07-09 13:05:20 -0300

    feat: receiving new parameters

--- src/hooks/useRemoveProduct.ts
+++ src/hooks/useRemoveProduct.ts
@@ -4,10 +4,11 @@ import { CreateUser } from '../types/entities';
 export async function removeProduct(
   userId: string | null,
   productId: string | undefined,
+  imageName: string | undefined,
   token: string | null
 ) {
   const res = await httpClient<CreateUser>({
-    url: `/delete/product/${userId}/${productId}`,
+    url: `/delete/product/${userId}/${productId}/${imageName}`,
     method: 'DELETE',
     headers: {
       authorization: `Bearer ${token}`,
