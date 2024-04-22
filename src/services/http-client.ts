a3a822beb199dfae786f78eb64a659e84c602cfc
Hamudi <ramonramos.silva16@gmail.com>
2024-04-22 19:01:57 -0300

    feat: modify api url

--- src/services/http-client.ts
+++ src/services/http-client.ts
@@ -3,7 +3,7 @@ import axios, { AxiosPromise, AxiosRequestConfig } from 'axios';
 const url =  "https://apiecommercerhamudi.onrender.com";
 const dev = "http://localhost:4000/";
 const axiosInstance = axios.create({
-  baseURL: dev,
+  baseURL: url,
 });
 
 export const httpClient = <T>(config: AxiosRequestConfig) => {
